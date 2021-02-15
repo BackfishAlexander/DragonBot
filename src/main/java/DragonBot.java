4d793abbfeddd120b634e47b745d67f301c1fc3f
BackfishAlexander <54977515+BackfishAlexander@users.noreply.github.com>
2021-02-15 14:08:46 -0500

    Fixed exposing my token

--- src/main/java/DragonBot.java
+++ src/main/java/DragonBot.java
@@ -1,12 +1,17 @@
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
+import java.io.FileInputStream;
+
 public class DragonBot {
     public static JDA jda;
     public static String prefix = "/";
     public static void main(String[] args) throws Exception {
 
-        jda = JDABuilder.createDefault("Nzc1NTAxMzk3NTA3OTY0OTc5.X6nP9Q.IDQFzQR3YRkKjboiKlwgStqfDlo").build();
+        FileInputStream fis = new FileInputStream("token.txt"); //Gamer fact: Don't upload your discord token to github lol
+
+        String token = fis.toString();
+        jda = JDABuilder.createDefault(token).build();
         jda.addEventListener(new commands());
 
         //new Scraper();
